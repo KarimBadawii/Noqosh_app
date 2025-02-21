b5ebf6b0d776a0e13ff2f4597ab538b4c8a9c389
Adham <aadham.amr@gmail.com>
2025-02-21 17:19:43 +0200

    auth ui

--- lib/core/utils/font_manager.dart
+++ lib/core/utils/font_manager.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class FontConstants {
-  static const String fontFamily = "Poppins";
+  static const String fontFamily = "Playfair";
 }
 
 class FontWeightManager {
@@ -19,6 +19,7 @@ class FontSize {
   static const double s17 = 17.0;
   static const double s18 = 18.0;
   static const double s20 = 20.0;
-  static const double s22 = 22.0;
-  static const double s24 = 24.0;
+  static const double s25 = 25.0;
+  static const double s35 = 35.0;
+  static const double s50 = 50.0;
 }
